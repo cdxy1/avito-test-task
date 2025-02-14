e000c3987375d0c63bd2b7bb938f4e9bdc478399
Artem <dalyvinchi@gmail.com>
2025-02-14 16:05:41 +0300

    Reorder parameters in get_item_by_name function for clarity.

--- app/utils/transaction_utils.py
+++ app/utils/transaction_utils.py
@@ -6,7 +6,7 @@ from ..models.item import ItemModel
 from ..models.user import UserModel
 
 
-async def get_item_by_name(session: AsyncSession, item_name: str) -> ItemModel:
+async def get_item_by_name(item_name: str, session: AsyncSession) -> ItemModel:
     query = select(ItemModel).filter(ItemModel.name == item_name)
     result = await session.execute(query)
     item = result.scalars().first()
